5fccdfabb0a2ac01d8958534acf0c140a376a3d3
gillKSC <77927150+gillKSC@users.noreply.github.com>
2022-02-18 23:32:24 -0500

    Update models.py

--- hw2_sp2022/models.py
+++ hw2_sp2022/models.py
@@ -35,16 +35,35 @@ class LogisticRegressionSGD(Model):
 
     def __init__(self, n_features, learning_rate = 0.1):
         super().__init__()
-        # TODO: Initialize parameters, learning rate
+        self.n_features = n_features
+        self.learning_rate = learning_rate
         pass
 
     def fit(self, X, y):
-        # TODO: Write code to fit the model
+        m = np.zeros_like(X[0])
+        epochs = 50  # no. of iterations for optimization
+
+        # Performing Gradient Descent Optimization
+        # for every epoch
+        for epoch in range(1, epochs + 1):
+            # for every data point(X_train,y_train)
+            for i in range(len(X)):
+                # compute gradient w.r.t 'm'
+                gr_wrt_m = X[i] * (y[i] - sigmoid(np.dot(self.n_features.T, X[i])))
+                # update m, c
+                self.n_features = self.n_features - self.learning_rate * gr_wrt_m
         pass
 
     def predict(self, X):
-        # TODO: Write code to make predictions
-        pass
+        predictions = []
+        for i in range(len(X)):
+            z = np.dot(self.n_features, X[i])
+            y_pred = sigmoid(z)
+            if y_pred >= 0.5:
+                predictions.append(1)
+            else:
+                predictions.append(0)
+        return predictions
 
 class LogisticRegressionNewton(Model):
 
