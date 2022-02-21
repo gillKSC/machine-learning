d8f5a94754a19503b2ec52737553094869922c5f
gillKSC <77927150+gillKSC@users.noreply.github.com>
2022-02-21 01:09:01 -0500

    Update models.py

--- hw2_sp2022/models.py
+++ hw2_sp2022/models.py
@@ -129,7 +129,7 @@ class LogisticRegressionNewton(Model):
         #print(self.beta.shape)
 
     def predict(self, X):
-        beta = column(self.beta, 0)
+        beta = column(self.beta, 1)
         X = X.todense()
         X = fix_test_feats(X, self.n_features+1)
         linear_model = np.dot(X, beta)
