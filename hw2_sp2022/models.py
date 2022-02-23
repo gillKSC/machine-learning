2eb2fc63ddd25058b9e646c3d9d603be54468e68
gillKSC <77927150+gillKSC@users.noreply.github.com>
2022-02-23 00:44:24 -0500

    Update models.py

--- hw2_sp2022/models.py
+++ hw2_sp2022/models.py
@@ -106,7 +106,7 @@ class LogisticRegressionNewton(Model):
         hessian = np.zeros((X.shape[1],X.shape[1]))
         for i in range(hessian.shape[0]):
             for j in range(hessian.shape[1]):
-                hessia_ij = np.mean(np.multiply(h, (1-h)) * np.multiply(X[:,i], X[:,j]))
+                hessia_ij = np.mean(np.multiply(h, (1-h)) @ np.multiply(X[:,i], X[:,j]))
                 hessian[i,j] = hessia_ij
         """
         secDerivLogL = np.identity(n_features)
